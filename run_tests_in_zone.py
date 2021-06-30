58f77b2d0d91a60bfd0eeab11fbb8c4049fd71c6
Alan King <alanking@renci.org>
2021-06-30 12:13:05 -0400

    Add support for list of commands

--- run_tests_in_zone.py
+++ run_tests_in_zone.py
@@ -1,12 +1,14 @@
 import docker
 
-FLAG_FILE = '/var/lib/irods/setup_complete'
 
 class execution_context:
+    FLAG_FILE = '/var/lib/irods/setup_complete'
+    OUTPUT_ENCODING = 'utf-8'
+
     def __init__(self, project_name, args, dc):
         self.project_name   = project_name
         self.container_name = args.container
-        self.commands       = args.command
+        self.commands       = list(args.commands)
         self.setup_timeout  = args.setup_timeout
         self.docker_client  = dc
 
@@ -16,17 +18,38 @@ def wait_for_setup_to_finish(dc, c, timeout_in_seconds):
     print('waiting for iRODS to finish setting up [{}]'.format(c.name))
 
     start_time = time.time()
+    now = start_time
 
-    while time.time() - start_time < timeout_in_seconds:
-        exec_result = c.exec_run('stat {}'.format(FLAG_FILE))
+    while now - start_time < timeout_in_seconds:
+        exec_result = c.exec_run('stat {}'.format(execution_context.FLAG_FILE))
 
-        if exec_result[0] == 0:
-            print('iRODS has been set up - continuing execution')
+        if exec_result.exit_code == 0:
+            print('iRODS has been set up (waited [{}] seconds)'.format(str(now - start_time)))
             return
 
         time.sleep(1)
+        now = time.time()
 
     raise SetupTimeoutError('timed out while waiting for iRODS to finish setting up')
+
+def execute_command_with_output(c, command):
+    ec = 0
+
+    # iRODS tests are meant to be run as the irods service account in the /var/lib/irods directory
+    # TODO: loop through commands and run them serially
+    exec_out = c.exec_run(command, user='irods', workdir='/var/lib/irods', stream=True)
+
+    try:
+        # Stream output from the executing command
+        while True:
+            print(next(exec_out.output).decode(execution_context.OUTPUT_ENCODING), end='')
+
+        ec = exec_out.exit_code
+
+    except StopIteration:
+        print('\ndone')
+
+    return ec
 
 def execute_on_project(ctx):
     import compose.cli.command
@@ -46,18 +69,12 @@ def execute_on_project(ctx):
         # Ensure that iRODS setup has completed
         wait_for_setup_to_finish(ctx.docker_client, c, ctx.setup_timeout)
 
-        # iRODS tests are meant to be run as the irods service account in the /var/lib/irods directory
-        # TODO: loop through commands and run them serially
-        exec_out = c.exec_run(ctx.commands, user='irods', workdir='/var/lib/irods', stream=True)
+        # TODO: install desired version here
 
-        try:
-            # Stream output from the executing command
-            while True: print(str(next(exec_out.output)))
-
-            ec = exec_out.exit_code
-
-        except StopIteration:
-            print('reached end of stream')
+        for command in ctx.commands:
+            print('----\nexecuting command [{}]'.format(command))
+            ec = execute_command_with_output(c, command)
+            print('\nexecution complete\n----')
 
     except Exception as e:
         print(e)
@@ -78,8 +95,8 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Run iRODS tests in a consistent environment.')
-    parser.add_argument('command', metavar='COMMAND', type=str,
-                        help='The string representing the command to be run in the container')
+    parser.add_argument('commands', metavar='COMMANDS', nargs='+',
+                        help='Space-delimited list of commands to be run')
     parser.add_argument('--container', metavar='CONTAINER', type=str,
                         default='irods_test_base_irods-catalog-provider_1',
                         help='The name of the container on which the command will run')
