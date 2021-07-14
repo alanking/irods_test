53fa5ac7133354e889b8fd4df0fb9ecb6944fb63
Alan King <alanking@renci.org>
2021-07-14 17:23:56 -0400

    fixed ODBC driver issues on centos (still need to understand what's going wrong)

--- projects/centos-7-mysql-5.7/provider/start_provider.sh
+++ projects/centos-7-mysql-5.7/provider/start_provider.sh
@@ -9,6 +9,15 @@ until nc -z catalog.example.org 3306; do
 done
 echo Postgres took approximately $counter seconds to fully start ...
 
+# Instructions from https://dev.mysql.com/doc/connector-odbc/en/connector-odbc-installation-binary-unix-tarball.html
+# TODO: Is this needed?
+cp /mysql-connector-odbc-5.2.7-linux-el6-x86-64bit/lib/* /usr/lib64
+cp /mysql-connector-odbc-5.2.7-linux-el6-x86-64bit/bin/* /usr/bin
+
+# This is needed in order for the older MySQL ODBC connector to work (TODO: Verify)
+ln -s /usr/lib64/libodbc.so.2.0.0 /usr/lib64/libodbc.so.1
+ln -s /usr/lib64/libodbcinst.so.2.0.0 /usr/lib64/libodbcinst.so.1
+
 # Set up iRODS if not already done
 if [ ! -e /var/lib/irods/setup_complete ]
     then
