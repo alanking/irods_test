a92161be765fd44f6380bf86e8c7f8abd3889f66
Alan King <alanking@renci.org>
2021-08-25 16:01:09 -0400

    add strategy for setting up iRODS catalog in database service

--- context.py
+++ context.py
@@ -1,3 +1,12 @@
+def irods_catalog_database_service():
+    return 'catalog'
+
+def irods_catalog_provider_service():
+    return 'irods-catalog-provider'
+
+def irods_catalog_consumer_service():
+    return 'irods-catalog-consumer'
+
 def project_name(container_name):
     return container_name.split('_')[0]
 
@@ -14,14 +23,23 @@ def image_name_and_tag(image_tag):
     return image_tag.split(':')
 
 def image_name(image_tag):
-    return image_name_and_version(image_tag)[0]
+    return image_name_and_tag(image_tag)[0]
 
-def image_version(image_tag):
-    return image_name_and_version(image_tag)[1]
+def image_tag(image_tag):
+    return image_name_and_tag(image_tag)[1]
+
+def irods_catalog_provider_container(project_name, service_instance=1):
+    return container_name(project_name, irods_catalog_provider_service(), service_instance)
+
+def irods_catalog_consumer_container(project_name, service_instance=1):
+    return container_name(project_name, irods_catalog_consumer_service(), service_instance)
+
+def irods_catalog_database_container(project_name, service_instance=1):
+    return container_name(project_name, irods_catalog_database_service(), service_instance)
 
 def is_catalog_database_container(container):
-    return service_name(container.name) == 'catalog'
+    return service_name(container.name) == irods_catalog_database_service()
 
 def is_catalog_service_provider_container(container):
-    return service_name(container.name) == 'irods-catalog-provider'
+    return service_name(container.name) == irods_catalog_provider_service()
 
--- setup.py
+++ setup.py
@@ -8,25 +8,95 @@ import os
 import context
 import execute
 
-# TODO: Strategy for different databases
+class database_setup_strategy(object):
+    def __init__(self, container):
+        self.container = container
+
+    def execute_psql_command(self, psql_cmd):
+        raise NotImplementedError('method not implemented for database strategy')
+
+    def create_database(self, name):
+        raise NotImplementedError('method not implemented for database strategy')
+
+    def create_user(self, username, password):
+        raise NotImplementedError('method not implemented for database strategy')
+
+    def elevate_privileges(self, database, username):
+        raise NotImplementedError('method not implemented for database strategy')
+
+    def drop_database(self, name):
+        raise NotImplementedError('method not implemented for database strategy')
+
+    def drop_user(self, username):
+        raise NotImplementedError('method not implemented for database strategy')
+
+class postgres_database_setup_strategy(database_setup_strategy):
+    def __init__(self, container):
+        super().__init__(container)
+
+    def execute_psql_command(self, psql_cmd):
+        cmd = 'psql -c \"{}\"'.format(psql_cmd)
+        return execute.execute_command(self.container, cmd, user='postgres')
+
+    def create_database(self, name):
+        return self.execute_psql_command(
+            'create database \\\"{}\\\";'.format(name))
+
+    def create_user(self, username, password):
+        return self.execute_psql_command(
+            'create user {0} with password \'{1}\';'.format(username, password))
+
+    def elevate_privileges(self, database, username):
+        return self.execute_psql_command(
+            'grant all privileges on database \\\"{0}\\\" to {1};'.format(database, username))
+
+    def drop_database(self, name):
+        return self.execute_psql_command(
+            'drop database \\\"{}\\\";'.format(name))
+
+    def drop_user(self, username):
+        return self.execute_psql_command(
+            'drop user {};'.format(username))
+
+    def list_databases(self):
+        return self.execute_psql_command('\l')
+
+def make_database_setup_strategy(database, container):
+    suffix = '_database_setup_strategy'
+    return eval(context.image_name(database) + suffix)(container)
+
 def setup_catalog(docker_client,
                   project_name,
-                  catalog_service_name='catalog',
-                  instance_number=1,
+                  database_tag,
+                  service_instance=1,
                   database_name='ICAT',
                   database_user='irods',
                   database_password='testpassword'):
 
-    container_name = context.container_name(project_name, catalog_service_name, str(instance_number))
+    container_name = context.irods_catalog_database_container(project_name, service_instance)
+
     container = docker_client.containers.get(container_name)
 
-    create_database = 'create database \\\"{}\\\";'.format(database_name)
-    create_user = 'create user {0} with password \'{1}\';'.format(database_user, database_password)
-    elevate_privileges = 'grant all privileges on database \\\"{0}\\\" to {1};'.format(database_name, database_user)
+    strat = make_database_setup_strategy(database_tag, container)
 
-    for psql_cmd in [create_database, create_user, elevate_privileges, '\l']:
-        cmd = 'psql -c \"{}\"'.format(psql_cmd)
-        execute.execute_command(container, cmd, user='postgres')
+    ec = strat.create_database(database_name)
+    if ec is not 0:
+        return ec
+
+    ec = strat.create_user(database_user, database_password)
+    if ec is not 0:
+        #strat.drop_database(database_name)
+        return ec
+
+    ec = strat.elevate_privileges(database_name, database_user)
+    if ec is not 0:
+        #strat.drop_database(database_name)
+        #strat.drop_user(database_user)
+        return ec
+
+    strat.list_databases()
+
+    return 0
 
 if __name__ == "__main__":
     import argparse
@@ -37,6 +107,10 @@ if __name__ == "__main__":
                         help='Path to the docker-compose project on which packages will be installed.')
     parser.add_argument('--project-name', metavar='PROJECT_NAME', type=str, dest='project_name',
                         help='Name of the docker-compose project on which to install packages.')
+    parser.add_argument('--database-tag', '-d', metavar='DATABASE_IMAGE_TAG', dest='database', type=str, default='postgres:10.12',
+                        help='The tag of the database container to use (e.g. postgres:10.12')
+    parser.add_argument('--exclude-catalog-setup', dest='setup_catalog', action='store_false',
+                        help='If indicated, skips the setup of iRODS tables and postgres user in the database.')
     parser.add_argument('--verbose', '-v', dest='verbosity', action='count', default=1,
                         help='Increase the level of output to stdout. CRITICAL and ERROR messages will always be printed.')
 
@@ -49,10 +123,12 @@ if __name__ == "__main__":
     p = compose.cli.command.get_project(os.path.abspath(args.project_path), project_name=args.project_name)
 
     try:
-        ec = setup_catalog(docker_client, p.name)
+        if args.setup_catalog:
+            logging.debug('setting up catalog [{}]'.format(p.name))
+            ec = setup_catalog(docker_client, p.name, args.database)
 
-        if ec is not 0:
-            exit(ec)
+            if ec is not 0:
+                exit(ec)
 
     except Exception as e:
         logging.critical(e)
